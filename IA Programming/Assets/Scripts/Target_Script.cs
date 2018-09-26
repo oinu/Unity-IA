8efbc0df246e4f5819a74d7e921525cb0bd4118a
oinu <oinu69@gmail.com>
2018-09-26 12:43:18 +0200

    Seek implemented

---
+++ IA Programming/Assets/Scripts/Target_Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target_Script : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       if(Input.GetAxis("Horizontal")>0)
+        {
+            this.transform.position = new Vector3(this.transform.position.x + speed, this.transform.position.y, this.transform.position.z);
+        }
+       else if(Input.GetAxis("Horizontal") < 0)
+        {
+            this.transform.position = new Vector3(this.transform.position.x - speed, this.transform.position.y, this.transform.position.z);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - speed);
+        }
+
+    }
+}

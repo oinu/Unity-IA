95a9e369cedad50f06dd39b73837b7118be0a0a5
oinu <oinu69@gmail.com>
2018-09-26 13:34:14 +0200

    Flee end and working in Arrive
    Signed-off-by: oinu <oinu69@gmail.com>

---
+++ IA Programming/Assets/Scripts/Arrive.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrive : MonoBehaviour {
+
+    public GameObject target;
+    public float maxForce;
+    public float maxSpeed;
+    private Vector3 desiredVelocity;
+    private Vector3 steeringForce;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        desiredVelocity = target.transform.position - this.transform.position;
+        this.transform.forward = desiredVelocity;
+        
+
+        steeringForce = desiredVelocity;
+        float distance = Vector3.Distance(target.transform.position, this.transform.position);
+        if(distance> target.transform.localScale.x)
+        {
+            steeringForce /= maxSpeed;
+        }
+        
+        steeringForce *= maxForce;
+
+        this.transform.position = new Vector3(this.transform.position.x + steeringForce.x, this.transform.position.y, this.transform.position.z + steeringForce.z);
+    }
+}
---
+++ IA Programming/Assets/Scripts/Flee.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flee : MonoBehaviour {
+
+    public GameObject target;
+    public float maxForce;
+    public float maxSpeed;
+    private Vector3 desiredVelocity;
+    private Vector3 steeringForce;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        desiredVelocity = this.transform.position - target.transform.position;
+
+        this.transform.forward = desiredVelocity;
+
+        steeringForce = desiredVelocity;
+        steeringForce /= maxSpeed;
+        steeringForce *= maxForce;
+
+        this.transform.position = new Vector3(this.transform.position.x + steeringForce.x, this.transform.position.y, this.transform.position.z + steeringForce.z);
+    }
+}

928b70b774984d1f0a31e23c756030a7791296ba
oinu <oinu69@gmail.com>
2019-01-07 16:05:10 +0100

    Navigation Mesh done! Multiselection agent.
    Signed-off-by: oinu <oinu69@gmail.com>

--- IA Programming/Assets/Scripts/Others/NavMeshScript.cs
+++ IA Programming/Assets/Scripts/Others/NavMeshScript.cs
@@ -1,26 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.AI;
 
 public class NavMeshScript : MonoBehaviour {
 
     public Camera cam;
+    public GameObject zone;
     public List<GameObject> agents;
     private List<GameObject> selectedAgents;
+    private Vector3 mouseClickDownScreenPosition, mouseClickUpScreenPosition;
+    private bool rightDirection;
     
 	// Use this for initialization
 	void Start () {
         selectedAgents = new List<GameObject>();
-        //selectedAgents = agents;
+        rightDirection = false;
     }
-	
+
 	// Update is called once per frame
 	void Update () {
 
+        
         //When the player click with left button
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            mouseClickDownScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+            zone.SetActive(true);
+            
             //Send a ray
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -51,6 +59,55 @@ public class NavMeshScript : MonoBehaviour {
             }
         }
 
+        //When the mouse button held pressed
+        if(Input.GetMouseButton(0))
+        {
+            //Update the second position
+            mouseClickUpScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+
+            //Re-size the area of zone
+            zone.GetComponent<RectTransform>().sizeDelta = new Vector2(Mathf.Abs(mouseClickDownScreenPosition.x - mouseClickUpScreenPosition.x)
+                , Mathf.Abs(mouseClickDownScreenPosition.y - mouseClickUpScreenPosition.y));
+
+            //Moving the image respecting the mouse position
+            Vector3 aux, pos;
+
+            float x = mouseClickDownScreenPosition.x - mouseClickUpScreenPosition.x;
+            float y = mouseClickDownScreenPosition.y - mouseClickUpScreenPosition.y;
+
+            aux = cam.ScreenToViewportPoint(mouseClickDownScreenPosition);
+
+            if (x > 0 && y > 0)
+            {
+                pos = new Vector3(-zone.GetComponent<RectTransform>().rect.width / 2 + cam.pixelWidth * aux.x,
+                 cam.pixelHeight * aux.y - zone.GetComponent<RectTransform>().rect.height / 2, 0);
+                rightDirection = false;
+            }
+            else if (x < 0 && y > 0)
+            {
+                pos = new Vector3(zone.GetComponent<RectTransform>().rect.width / 2 + cam.pixelWidth * aux.x,
+                cam.pixelHeight * aux.y - zone.GetComponent<RectTransform>().rect.height / 2, 0);
+                rightDirection = true;
+            }
+            else if (x < 0 && y < 0)
+            {
+                pos = new Vector3(zone.GetComponent<RectTransform>().rect.width / 2 + cam.pixelWidth * aux.x,
+                cam.pixelHeight * aux.y + zone.GetComponent<RectTransform>().rect.height / 2, 0);
+                rightDirection = true;
+            }
+            else
+            {
+                pos = new Vector3(-zone.GetComponent<RectTransform>().rect.width / 2 + cam.pixelWidth * aux.x,
+                cam.pixelHeight * aux.y + zone.GetComponent<RectTransform>().rect.height / 2, 0);
+                rightDirection = false;
+            }
+
+            zone.GetComponent<RectTransform>().position = pos;
+
+            SelectAgentsWithRange();
+        }
+
+
         //When the player click with the right button
         else if(Input.GetMouseButtonDown(1))
         {
@@ -66,7 +123,12 @@ public class NavMeshScript : MonoBehaviour {
             }
             
         }
-		
+
+        //When the player release click the left button
+        if(Input.GetMouseButtonUp(0))
+        {
+            zone.SetActive(false);
+        }
 	}
 
     /// <summary>
@@ -79,5 +141,92 @@ public class NavMeshScript : MonoBehaviour {
         {
             agent.GetComponent<NavMeshAgent>().SetDestination(position);
         }
+    }
+
+    //Select the agents inside the zone
+    void SelectAgentsWithRange()
+    {
+        //Send a ray starting in the click down position
+        Ray ray = cam.ScreenPointToRay(mouseClickDownScreenPosition);
+        RaycastHit hit;
+        Vector3 firstPosition,secondPosition;
+
+        //If the ray hist with some object
+        if (Physics.Raycast(ray, out hit))
+        {
+            firstPosition = hit.point;
+        }
+        else firstPosition = Vector3.zero;
+
+        //Calculet the world position in the actual mouse position (if the player don't click up the button
+        ray = cam.ScreenPointToRay(mouseClickUpScreenPosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            secondPosition = hit.point;
+        }
+        else secondPosition = Vector3.zero;
+
+        //If the first click is in to the left
+        if(rightDirection)
+        {
+            foreach(GameObject agent in agents)
+            {
+                //If is between the first and second position
+                if (agent.transform.position.x>firstPosition.x && agent.transform.position.x<secondPosition.x)
+                {
+                    if (agent.transform.position.z < firstPosition.z && agent.transform.position.z > secondPosition.z)
+                    {
+                        //Compare the color of the agent for not add the same agent in the list
+                        if(agent.GetComponent<Renderer>().material.color!=Color.green)
+                        {
+                            agent.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+                            selectedAgents.Add(agent);
+                        }
+                    }
+                    else if (agent.transform.position.z < secondPosition.z && agent.transform.position.z > firstPosition.z)
+                    {
+                        //Compare the color of the agent for not add the same agent in the list
+                        if (agent.GetComponent<Renderer>().material.color != Color.green)
+                        {
+                            agent.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+                            selectedAgents.Add(agent);
+                        }
+                    }
+                }
+            }
+        }
+
+        //If the first click is in to the right
+        else
+        {
+            foreach (GameObject agent in agents)
+            {
+                //If is between the first and second position
+                if (agent.transform.position.x < firstPosition.x && agent.transform.position.x > secondPosition.x)
+                {
+                    if (agent.transform.position.z < firstPosition.z && agent.transform.position.z > secondPosition.z)
+                    {
+                        //Compare the color of the agent for not add the same agent in the list
+                        if (agent.GetComponent<Renderer>().material.color != Color.green)
+                        {
+                            agent.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+                            selectedAgents.Add(agent);
+                        }
+                    }
+                    else if (agent.transform.position.z < secondPosition.z && agent.transform.position.z > firstPosition.z)
+                    {
+                        //Compare the color of the agent for not add the same agent in the list
+                        if (agent.GetComponent<Renderer>().material.color != Color.green)
+                        {
+                            agent.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+                            selectedAgents.Add(agent);
+                        }
+                    }
+                }
+            }
+        }
+
+        
     }
 }

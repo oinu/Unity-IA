3a531e0c2a5b3dc7addfa5f0edb4e4f8b8bebf5d
Eloi Salles <oinu69@gmail.com>
2019-12-16 20:50:26 +0100

    Create a Utility System

---
+++ IA Programming/Assets/Scripts/Decision Making/Utility_Based.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility_Based : MonoBehaviour
+{
+    public AnimationCurve hungry, pee, sleep, entertainment, socialize;
+    private GameObject bathroom, kitchen, bed, tv, friend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
